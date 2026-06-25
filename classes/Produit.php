7bfde19cf92a8d6819c819e436282adee63d978c
Fpied <piedfrancois@laposte.net>
2026-06-25 14:20:17 +0200

    Fin de la class Produit

--- classes/Produit.php
+++ classes/Produit.php
@@ -1,5 +1,27 @@
 <?php
 
 abstract class Produit{
+    protected $nom;
+    protected $prixHT;
+
+    public function __construct($nom, $prixHT)
+    {
+        $this->nom = $nom;
+        $this->prixHT = $prixHT;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    abstract function calculerPrixTTC();
+
+    public function getFraisDePort(){
+        return 0;
+    }
+
+
+
+
 
 }
